9796a1cefaac08b099e581c2bf7813a82524b275
Furkan Enes Apaydın <furkan.apaydin@ceng.deu.edu.tr>
2020-12-19 15:32:09 +0300

    added top donator getter to user analytics

--- app/Models/UserAnalytics.php
+++ app/Models/UserAnalytics.php
@@ -11,4 +11,12 @@ class UserAnalytics extends Model
     public function getUser(){
         return $this->belongsTo(User::Class);
     }
+
+    public function getTopDonator(){
+        if(!$this->topDonator)
+            return null;
+
+        $donator=Donator::find($this->top_donator_id);
+        return $donator;
+    }
 }
